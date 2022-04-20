79907ee2c13060d162b835af63e18ac6d8858085
balajim0 <103865288+balajim0@users.noreply.github.com>
2022-04-20 19:16:23 +0530

    Add files via upload

---
+++ PostiveOrNegative.java
@@ -0,0 +1,16 @@
+package week1.day1;
+
+public class PostiveOrNegative {
+	public static void main(String[] args) {
+		int num = 35;
+		if (num > 0) {
+			System.out.println("The number is postive");
+		} else if (num < 0) {
+
+			System.out.println(" The number is negative");
+		} else
+			System.out.println("The number is neither postive nor negative");
+
+	}
+
+}
